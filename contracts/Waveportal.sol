1fb8fcfa4d48c9531092291b3b95d185069302b9
David Adeleye <david@Davids-MacBook-Air.local>
2021-11-22 22:10:23 +0100

    updates to wave portal smart contract

--- contracts/Waveportal.sol
+++ contracts/Waveportal.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
 
 contract Waveportal {
+    constructor(){
+        console.log("yo, yo, i'm a contract and i'm smart");
+    }
 
 }
