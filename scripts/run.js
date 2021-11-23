bcdd4fbbadc7a53fb2ba68ab7fb27d5db426c115
David Adeleye <david@Davids-MacBook-Air.local>
2021-11-23 05:07:46 +0100

    updated contract with function to wave and get total waves

--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,5 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const wavePortalContractFactory = await hre.ethers.getContractFactory(
     "WavePortal"
   );
@@ -6,6 +7,13 @@ const main = async () => {
   await waveContract.deployed();
 
   console.log("Contract deployed to: ", waveContract.address);
+  console.log("Contract deployed by: ", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
