c647c2e008484f0cae9a87d3239012f9fd0d69df
codeddave <drizzzydave@gmail.com>
2021-11-27 07:08:47 +0100

    updated contract to send eth to every waver

--- contracts/Waveportal.sol
+++ contracts/Waveportal.sol
@@ -21,7 +21,7 @@ contract WavePortal {
     
 Wave[] waves;
 
-    constructor(){
+    constructor() payable{
         console.log(" i'm a smart contract and i'm smart");
     }
 
@@ -33,7 +33,15 @@ Wave[] waves;
             waves.push(Wave(msg.sender, block.timestamp, _message));
 
             emit NewWave(msg.sender, block.timestamp, _message);
-    }
+
+            uint256 prizeAmount = 0.0001 ether;
+         require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+        }
     function getAllWaves() public view returns (Wave[] memory){
         return waves;
     }
